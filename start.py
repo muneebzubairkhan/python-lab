6348b8fedf6f6a8294835403d8ebcdcb038da64b
timon#1213 <money.bird.mi@gmail.com>
2021-08-31 19:30:22 +0500

    saving thumbnails in 2 folders

--- start.py
+++ start.py
@@ -1,8 +1,16 @@
 from PIL import Image
 import os
 
+size_300 = (300, 300)
+size_700 = (700, 700)
+
 for f in os.listdir('.'):
     if f.endswith('.png'):
         i = Image.open(f)
         fn, fext = os.path.splitext(f)
-        i.save('webp_folder/' + fn + '.webp')
+
+        i.thumbnail(size_300)
+        i.save('webp_size_300/' + fn + '.webp')
+
+        i.thumbnail(size_700)
+        i.save('webp_size_700/' + fn + '.webp')
