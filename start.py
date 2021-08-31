637f59615d3e6a960543f25b3df757a6b5ee22f7
timon#1213 <money.bird.mi@gmail.com>
2021-08-31 19:34:42 +0500

    rotate image

--- start.py
+++ start.py
@@ -1,16 +1,4 @@
 from PIL import Image
-import os
 
-size_300 = (300, 300)
-size_700 = (700, 700)
-
-for f in os.listdir('.'):
-    if f.endswith('.png'):
-        i = Image.open(f)
-        fn, fext = os.path.splitext(f)
-
-        i.thumbnail(size_300)
-        i.save('webp_size_300/' + fn + '.webp')
-
-        i.thumbnail(size_700)
-        i.save('webp_size_700/' + fn + '.webp')
+i = Image.open('pup1.png')
+i.rotate(45).save('pup1_rotate1.webp')
